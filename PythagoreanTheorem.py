5c693d26003e53d6326a11e88833f8fcc707a1e4
FruitJess04 <113867832+FruitJess04@users.noreply.github.com>
2022-12-06 23:21:42 +0800

    Add files via upload

---
+++ PythagoreanTheorem.py
@@ -0,0 +1,89 @@
+
+import math
+
+
+class Pythagorean:
+    
+    def side_c(side_a,side_b):
+        answer = math.sqrt((side_a*side_a)+(side_b*side_b))
+        print("Answer: sqrt("+ str(side_a) + " * " + str(side_a) + ") + (" +str(side_b) + " * " + str(side_b)+") = " +str(answer) + "      |")
+    # end def
+
+    def side_a(hypo_c, side_b):
+        answer = math.sqrt((hypo_c * hypo_c) - (side_b * side_b))
+        print("Answer: sqrt("+ str(hypo_c) + " * " + str(hypo_c) + ") - (" +str(side_b) + " * " + str(side_b)+") = " +str(answer) + "      |")
+        
+    # end def
+
+    def side_b(hypo_c,side_a):
+        answer = math.sqrt((hypo_c * hypo_c - side_a * side_a))
+        print("Answer: sqrt("+ str(hypo_c) + " * " + str(hypo_c) + ") - (" +str(side_a) + " * " + str(side_a)+") = " +str(answer) + "      |")
+        
+    # end def
+    
+    def pythagorean():   
+        while True:
+            print("==============================================================================================")
+            print("Pythagorean theorem calculator! Calculate your triangle sides.")
+            print("Assume the sides are a, b, c and c is the hypotenuse (the side opposite the right angle")
+            print("==============================================================================================")
+
+            formula = input("Which side (a, b, c) do you wish to calculate? side -> ")
+
+            if formula == 'c' or formula == 'C':
+                print("========================================================")
+                try:
+                    side_a = int(input("Input the length of side a: "))
+                    side_b = int(input("Input the length of side b: "))
+                    print("The length of Hypotenuse c is: " )
+                    Pythagorean.side_c(side_a,side_b)
+                    print("========================================================")
+                except ValueError as err:
+                    print()
+                    print(" ===================================================")
+                    print("| "+ str(err) +" |")
+                    print("| PUT a Number not a LETTER or SYMBOL           |")
+                    print(" ===================================================")
+                    print()
+                    
+            elif formula == 'a' or formula == 'A':
+                print("========================================================")
+                try:
+                    hypo_c = int(input("Input the Hypotenuse c: "))
+                    side_b = int(input("Input the length of side b: "))
+                    print("The length of side a is: " )
+                    Pythagorean.side_a(hypo_c,side_b)
+                    print("========================================================")
+                    print()
+                except ValueError as err:
+                    print(" ===================================================")
+                    print("| "+ str(err) +" |")
+                    print("| PUT a Number not a LETTER or SYMBOL           |")
+                    print(" ===================================================")
+                    print()
+
+            elif formula == 'b' or formula == 'B':
+                print()
+                print("========================================================")
+                try:
+                    hypo_c = int(input("Input the Hypotenuse c: "))
+                    side_a = int(input("Input the length of side a: "))
+                    print("The length of side b is: " )
+                    Pythagorean.side_b(hypo_c,side_a)
+                    print("========================================================")
+                    print()
+                except ValueError as err:
+                    print()
+                    print(" ===================================================")
+                    print("| "+ str(err) +" |")
+                    print("| PUT a Number not a LETTER or SYMBOL           |")
+                    print(" ===================================================")
+                    print()
+            elif formula == 'e' or formula == 'E':
+                exit()
+            else:
+                print()
+                print("=======================================")
+                print('Please select a side between a, b, c')
+                print("=======================================")
+                print()
